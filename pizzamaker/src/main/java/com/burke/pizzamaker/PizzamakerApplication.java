707b9613c8f9c1a3fe05cd44dbdafae9625b96ee
khten <106553846+khten@users.noreply.github.com>
2022-10-08 12:07:56 -0700

    Spring initializer added

---
+++ pizzamaker/src/main/java/com/burke/pizzamaker/PizzamakerApplication.java
@@ -0,0 +1,13 @@
+package com.burke.pizzamaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzamakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzamakerApplication.class, args);
+	}
+
+}
